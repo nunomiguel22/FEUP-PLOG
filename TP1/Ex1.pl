3a553a433098206605116e7ab068d0c40642afb2
nunomiguel22 <37627936+nunomiguel22@users.noreply.github.com>
2020-09-30 18:08:39 +0100

    TP1 Ex1

---
+++ TP1/Ex1.pl
@@ -0,0 +1,18 @@
+male('Aldo Burrows').
+male('Licoln Burrows').
+male('Michael Scofield').
+male('LJ Burrows').
+
+female('Christina Rose Scofield').
+female('Sara Tancredi').
+female('Ella Scofield').
+female('Lisa Rix').
+
+parent('Lisa Rix', 'LJ Burrows').
+parent('Lincoln Burrows', 'LJ Burrows').
+parent('Aldo Burrows', 'Lincoln Burrows').
+parent('Christina Rose Scofield', 'Lincoln Burrows').
+parent('Michael Scofield', 'Ella Scofield').
+parent('Sara Tancredi', 'Ella Scofield').
+parent('Aldo Burrows', 'Michael Scofield').
+parent('Christina Rose Scofield', 'Michael Scofield').
